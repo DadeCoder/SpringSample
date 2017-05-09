09db2a63be9e838dde39db6534b14580aadd8e04
DadeCoder <dade_zhang@163.com>
2017-05-09 23:39:46 +0800

    add pom

---
+++ SpringBootDemo/src/main/java/com/dade/spring/TestRunController.java
@@ -0,0 +1,17 @@
+package com.dade.spring;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Dade on 2017/5/9.
+ */
+@RestController
+public class TestRunController {
+
+    @RequestMapping("/api/test")
+    String test(){
+        return "This is just a demo!";
+    }
+
+}
